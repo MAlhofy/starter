bfe89a3097cd4a8faa3e5057bf40fcebd956c825
MAlhofy <melhofy20@gmail.com>
2020-08-21 19:10:07 +0200

    3

---
+++ app/Console/Commands/expiration.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use app\users;
+use Illuminate\Console\Command;
+
+class expiration extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:expire';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'expire users every 5 minute automatically';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $users = user:: where('expire', 0)->get(); //collections of users
+        foreach ($users as $user) {
+            $user->update(['expire => 1']);
+        }
+    }
+}
